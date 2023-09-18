e5e7d1b8dfd84805df49db6642d0a58a056864de
Olegeg <bgpfnic@protonmail.com>
2023-09-18 18:25:31 +0300

    finish

--- #source/js/app.js
+++ #source/js/app.js
@@ -1,1 +1,3 @@
+"use strict"
+
 // @prepros-append burger-menu.js
--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-let menuBurger=document.querySelector(".menu__burger"),menuList=document.querySelector(".menu__list");menuBurger.addEventListener("click",(()=>{menuBurger.classList.toggle("_active"),menuList.classList.toggle("_active"),document.body.classList.toggle("_lock")}));
+"use strict";let menuBurger=document.querySelector(".menu__burger"),menuList=document.querySelector(".menu__list");menuBurger.addEventListener("click",(()=>{menuBurger.classList.toggle("_active"),menuList.classList.toggle("_active"),document.body.classList.toggle("_lock")}));
